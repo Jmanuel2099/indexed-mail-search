7ef496f5c67b30232d85c0d1329fac57a626eeaf
Jose Manuel Marin <jmanuel2099@gmail.com>
2023-02-08 00:05:45 -0500

    CORS are configured on the server

--- server/go.mod
+++ server/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/ajg/form v1.5.1 // indirect
 	github.com/dghubble/sling v1.4.1 // indirect
 	github.com/go-chi/chi/v5 v5.0.8 // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
 	github.com/go-chi/render v1.0.2 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 )
--- server/pkg/rest_server/router.go
+++ server/pkg/rest_server/router.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"github.com/go-chi/render"
 )
 
@@ -16,7 +17,21 @@ func (rs *RestServer) configureRouter() {
 	rs.Router.Use(middleware.Recoverer)
 	rs.Router.Use(middleware.URLFormat)
 	rs.Router.Use(render.SetContentType(render.ContentTypeJSON))
+	rs.configureCORS()
 	rs.configureRoutes()
+}
+
+// configureCORS setups the router to allow cross-source resource sharing
+// only with the VUE client and certain methods.
+func (rs *RestServer) configureCORS() {
+	rs.Router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://127.0.0.1:5173"},
+		AllowedMethods:   []string{"GET", "POST", "DELETE"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 }
 
 // configureRoutes sets up the routes for the app.
@@ -26,5 +41,5 @@ func (rs *RestServer) configureRoutes() {
 	})
 
 	rs.Router.Post("/indexer", rs.indexerHandler.IndexEmails)
-	rs.Router.Get("/search/{term}", rs.indexedSearchHandler.SearchTermInEmails)
+	rs.Router.Get("/search", rs.indexedSearchHandler.SearchTermInEmails)
 }
